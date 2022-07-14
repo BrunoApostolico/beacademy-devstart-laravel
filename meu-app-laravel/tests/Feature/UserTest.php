0d4d98879174f0d84a1a8be0acd594abffb4ef1b
BRUNO APOSTOLICO <bapostolico@gmail.com>
2022-07-14 18:38:36 -0300

    Teste Unit UserTest

---
+++ meu-app-laravel/tests/Feature/UserTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    public function test_user()
+    {
+        $user = User::factory()->create();
+        $response = $this->post('/login',[
+            'email' => $user->email,
+            'password'=> 'password'
+        ]);
+
+        $this->actingAs($user);
+        $response = $this->get('/users');
+
+        $response->assertStatus(200);
+    }
+}
